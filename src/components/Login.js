fc9ab684ae5fd8eb03a82273d269a2e25fdcc0f9
Matt Lane <lane.matthew@gmail.com>
2017-05-05 15:23:32 -0700

    styling login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { BASE_URL } from '../actions/auth';
 import { connect } from 'react-redux';
 import { mapStateForAuth } from '../helpers/connectHelpers';
+import background from '../background.jpg';
+import './Login.css'
 
 const Login = props => (
   <div>
-    <a href={`${BASE_URL}/login`}>Log in with Spotify</a>
-    <p>
-      {props.loginError}
-    </p>
+    <div id="main-image">
+      <img src={background} alt="Login page background"/>
+    </div>
+    <div id="content">
+      <h1>
+        <i className="fa fa-2x fa-arrow-circle-o-up" aria-hidden="true"></i>
+        Tune Up
+      </h1>
+      <p>Visualize your most recent favorite Spotify tracks.</p>
+      <p>Find new songs that you'll love.</p>
+      <a href={`${BASE_URL}/login`} className="login">
+        <i className="fa fa-3x fa-spotify" aria-hidden="true"></i>
+        Log in with Spotify
+      </a>
+      <p>
+        {props.loginError}
+      </p>
+    </div>
   </div>
 )
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import 'font-awesome/css/font-awesome.css';
 import './index.css';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
